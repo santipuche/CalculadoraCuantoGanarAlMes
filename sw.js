c374d2e9687574d5866394767ead97a9fa995671
santipuche <santipuche08@gmail.com>
2026-01-07 14:27:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'finanzas-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const assets = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
